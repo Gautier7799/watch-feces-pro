80ae14f2137f701147c4baffe6552bb609444829
GAIDI AMJED <150559809+Gautier7799@users.noreply.github.com>
2026-09-05 18:14:10 +0100

    Add dependencies for Jetpack Compose and Coroutines
    Added dependencies for Jetpack Compose and Coroutines.

---
+++ app/build.gradle.kts
@@ -0,0 +1,18 @@
+dependencies {
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.compose.material.icons.core)
+    implementation(libs.androidx.compose.material.icons.extended)
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.compose)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    // مكتبة الصور من الهاتف:
+    implementation(libs.coil.compose)
+    implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.kotlinx.coroutines.core)
+}
